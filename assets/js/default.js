7ec98eafbabf39669ed3dac1c99974097a846c60
Enes <enesdemirag1@gmail.com>
2020-02-16 19:02:31 +0300

    gitfolio

--- assets/js/default.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $('.item').each(function (index, value) { 
-        setInterval(function () {
-            $(value).css('margin-top', '8px');
-        }, 80 * index);
-    });
-});
