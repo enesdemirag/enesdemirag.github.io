bc512ef64ce9f1444d68586cf7d7072f25e9f981
enesdemirag <ensdmrg@gmail.com>
2025-12-04 16:08:27 +0300

    fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // GitHub Pages base URL
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
